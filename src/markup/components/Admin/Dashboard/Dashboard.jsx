a4cdb130d4347209b347d8ad7550355fc0930872
Hamza0141 <131574624+Hamza0141@users.noreply.github.com>
2024-09-18 06:08:51 -0500

    dashbord updated

--- src/markup/components/Admin/Dashboard/Dashboard.jsx
+++ src/markup/components/Admin/Dashboard/Dashboard.jsx
@@ -8,8 +8,8 @@ function Dashboard() {
         <div className="sec-title style-two">
           <h2>Dashboard</h2>
           <div className="text">
-            Experience the convenience and efficiency of all star, and discover a new level of
-            productivity and engagement in your work.
+            Experience the ease and efficiency of Regency, and unlock a new
+            level of productivity and engagement in your work.
           </div>
         </div>
         <div className="row">
@@ -18,10 +18,6 @@ function Dashboard() {
               <Link to="/admin/orders">
                 <h2>All Orders</h2>
               </Link>
-
-              <div className="icon">
-                <span className="flaticon-power"></span>
-              </div>
             </div>
           </div>
           <div className="col-lg-4 service-block-one">
@@ -29,10 +25,6 @@ function Dashboard() {
               <Link to="/admin/order">
                 <h2>New Orders</h2>
               </Link>
-
-              <div className="icon">
-                <span className="flaticon-gearbox"></span>
-              </div>
             </div>
           </div>
           <div className="col-lg-4 service-block-one">
@@ -40,45 +32,20 @@ function Dashboard() {
               <Link to="/admin/employees">
                 <h2>Employee</h2>
               </Link>
-
-              <div className="icon">
-                <span className="flaticon-brake-disc"></span>
-              </div>
             </div>
           </div>
           <div className="col-lg-4 service-block-one">
             <div className="inner-box hvr-float-shadow">
-              <h2>Engine Service & Repair</h2>
-              <div className="icon">
-                <span className="flaticon-car-engine"></span>
-              </div>
+              <Link to="/admin/services">
+                <h2>Services</h2>
+              </Link>
             </div>
           </div>
           <div className="col-lg-4 service-block-one">
             <div className="inner-box hvr-float-shadow">
-              <h2>Wheels</h2>
-              <div className="icon">
-                <span className="flaticon-tire"></span>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-4 service-block-one">
-            <div className="inner-box hvr-float-shadow">
-              <h2>Air Conditioning Evac</h2>
-
-              <div className="icon">
-                <span className="flaticon-air-conditioning"></span>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-4 service-block-one">
-            <div className="inner-box hvr-float-shadow">
-              <h2>General Service</h2>
-
-              <div className="icon">
-                <span className="flaticon-car-service"></span>
-              </div>
+              <Link to="/admin/Profile">
+                <h2>profile</h2>
+              </Link>
             </div>
           </div>
         </div>
